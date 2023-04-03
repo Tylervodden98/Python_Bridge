6c58cfbe2f214c5885d506e317c4a42642c9f57a
Tyler Vodden <vodden98@hotmail.com>
2023-04-03 15:15:53 -0400

    first commit

---
+++ print_exe.py
@@ -0,0 +1,7 @@
+#Python testing print/input
+
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+profession = input("Please enter your profession: ")
+
+print(f"Hi {name}! you are {age} years old, and your profession is a {profession}")
